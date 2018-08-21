0bc9f2eae208d53e14335e8d542c160e630519a6
harttle <yangjvn@126.com>
2018-08-21 16:43:20 +0800

    fix: umd

--- fake.js
+++ fake.js
@@ -1,6 +1,10 @@
-define('debug', function () {
-    function logger() {
-    }
+!function (factory) {
+  if("object" == typeof exports && "undefined" != typeof module)
+    module.exports = factory();
+  else if ("function" == typeof define && define.amd)
+    define([], factory)
+}(function () {
+    function logger() {}
     return function factory() {
         return logger;
     };
